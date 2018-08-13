3ca76610a599dbdaacb4058ca915afee4bada02d
fukkuen <fukkuen@gmail.com>
2018-08-13 19:07:58 +0800

    nuxt init

---
+++ pages/bio.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  .bio
+    p Wong Fuk Kuen was graduated from the School of Creative Media, City University of Hong Kong in 2012. Not until his final year during the course of the four years time in the school he discover a small artistic world and language that belongs to himself, which was manifested as a sound installation - The Telescope (2012), made of numerous water pipe that engage the audience listening to spacey sound of different tones.
+    p After his graduation, Fuk Kuen has been incidentally involved in different art projects from network rooted from college, and continue on exploring further thought and potential on his art, that would be forgotten otherwise.
+    p The Idea of State is the core thought of Fuk Kuen’s. Someone, or something participating in something. Ongoing. Not necessary with movement - Tension is a state. In 2016 had his first solo exhibition named Present Continuous, is originated from this idea.
+    p Force, speed, time and space are the concerns and the language of Fuk Kuen. While these elements sounds neutral and cold, his thought starts with a humanize idea. Paradox lives inside Fuk Kuen.
+</template>
+
+<style lang="stylus">
+
+</style>
