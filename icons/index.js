bab6a97c227b5d592a229ad41816f46e283ea56c
fukkuen <fukkuen@gmail.com>
2019-11-16 20:28:07 +0800

    add missing file

---
+++ icons/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable */
+require('./ig')
+require('./right')
---
+++ icons/right.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'right': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 36 36',
+    data: '<path pid="0" d="M31.48 19.244l-8.38 8.383 1.638 1.697L36 18.064 24.61 6.675l-1.64 1.697 8.512 8.51L0 16.886v2.36"/>'
+  }
+})
